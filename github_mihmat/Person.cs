139db8aa957aafd957d7797041d320354a85df98
Mustafa Mihmat <mihmat.m02@htlwienwest.at>
2020-12-23 14:37:15 +0100

    Bob hat Privatesetter erstellt

--- github_mihmat/Person.cs
+++ github_mihmat/Person.cs
@@ -12,8 +12,8 @@ namespace github_mihmat
             Lastname = ln;
         }
 
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string Firstname { get; private set; }
+        public string Lastname { get; private set; }
 
     }
 }
